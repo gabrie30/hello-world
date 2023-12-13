89b39ef840115dec0c200167d6c86496e7454ae0
Jay Gabriels <jgabriels30@gmail.com>
2023-12-13 11:20:40 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-docker build -t gabrie30/hello-world .
-docker push gabrie30/hello-world
+# Get the short Git SHA
+GIT_SHA=$(git rev-parse --short HEAD)
+
+# Docker image name
+DOCKER_IMAGE_NAME="gabrie30/hello-world"
+
+# Tag the Docker image
+docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:$GIT_SHA
+docker tag $DOCKER_IMAGE_NAME $DOCKER_IMAGE_NAME:latest
+
+# Push the tagged images to Docker Hub
+docker push $DOCKER_IMAGE_NAME:$GIT_SHA
+docker push $DOCKER_IMAGE_NAME:latest
